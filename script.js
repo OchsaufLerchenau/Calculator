5b133ce391879cc6fba9dbd42515eda92d4ef5b3
OchsaufLerchenau <maciej.burdzy@gmail.com>
2020-04-25 17:46:13 +0200

    finished basic functionality. To do: styling backspace and upper display, fixing backspace bugs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var multiInputValue = 0;
 var multDivCount = 0;
 var inputValue = '';
 var operator = '';
+var ans = 0;
 var multDivOperator = '';
 
 var functions = {
@@ -14,45 +15,101 @@ var functions = {
     divide: function(a, b) {return a/b}
 }
 
-function numberButtonPress(e) {
-    // if (/^[0-9]*\.[0-9]*$/.test(inputValue)) {
-    //     console.log('chuja')
-    //     document.getElementById('comma').className('buja')
-    // } 
-
-    inputValue += this.dataset.input.toString();
-
+function backspace() {
+    inputValue = Array.from(inputValue).pop().toString()
     displayValue = inputValue;
     display.textContent = displayValue;
 }
 
-function operatorButtonPress(e) {
-    if (this.dataset.input === 'add' || this.dataset.input === 'subtract') {
-        tempValueDisp = '';
-        
-        if (tempValueCount !== 0) {
-            tempValueCount = functions[operator](Number(tempValueCount), Number(inputValue));
-        } else if (tempValueCount === 0) {tempValueCount = inputValue}
-
-        tempValueDisp = this.textContent;
-        value.textContent = tempValueDisp;
-        inputValue = '';
-        display.textContent = inputValue;
-        operator = this.dataset.input.toString();
-
-    } else if (this.dataset.input === 'multiply' || this.dataset.input === 'divide') {
-        if (multDivCount !== 0) {
-            multDivCount = functions[multDivOperator](Number(multDivCount), Number(inputValue));
-            console.log(multDivCount)
-        } else if (multDivCount === 0) {multDivCount = inputValue; console.log(multDivCount)}
-
-        multDivOperator = this.dataset.input.toString();
-        tempValueDisp = this.textContent;
-        value.textContent = tempValueDisp;
-        inputValue = '';
-        display.textContent = inputValue; 
-    }
+var commaClick = function () {
+    inputValue += '.'
+    displayValue = inputValue;
+    display.textContent = displayValue;
+    comma.removeEventListener('click', commaClick);
 }
+
+function numberButtonPress(e) {
+    inputValue += this.dataset.input.toString();
+    displayValue = inputValue;
+    display.textContent = displayValue;
+}
+
+function addSubtractPress(e) {
+    
+    if (ans !== 0) {inputValue = ans}
+    tempValueDisp = '';
+    var r = 0
+    if (multDivCount === 0) {
+        r = inputValue
+    } else {
+        r = functions[multDivOperator](Number(multDivCount), Number(inputValue));
+        multDivCount = 0;
+    }
+
+    if (tempValueCount !== 0) {
+        tempValueCount = functions[operator](Number(tempValueCount), Number(r));
+    } else if (tempValueCount === 0) {tempValueCount = r}
+    console.log(tempValueCount);
+    tempValueDisp = this.textContent;
+    value.textContent = tempValueDisp;
+    inputValue = '';
+    display.textContent = inputValue;
+    operator = this.dataset.input.toString();
+    comma.addEventListener('click', commaClick);
+}
+
+function multDivPress(e) {
+    
+    if (multDivCount !== 0) {
+        multDivCount = functions[multDivOperator](Number(multDivCount), Number(inputValue));
+    } else if (multDivCount === 0) {multDivCount = inputValue;}
+
+    multDivOperator = this.dataset.input.toString();
+    tempValueDisp = this.textContent;
+    value.textContent = tempValueDisp;
+    inputValue = '';
+    display.textContent = inputValue;
+    comma.addEventListener('click', commaClick);
+}
+
+document.getElementById('equals').addEventListener('click', () => {
+    if (multDivCount === 0 && tempValueCount !== 0) {
+        tempValueCount = functions[operator](Number(tempValueCount), Number(inputValue));
+    } else if (multDivCount !== 0 && tempValueCount === 0) {
+        tempValueCount = functions[multDivOperator](Number(multDivCount), Number(inputValue));
+    } else if (multDivCount !== 0 && tempValueCount !== 0) {
+        tempValueCount = functions[operator](Number(tempValueCount),
+            Number(functions[multDivOperator](Number(multDivCount), Number(inputValue))));
+    } else {
+        tempValueCount = inputValue;
+    }
+    
+    tempValueCount = (Number(tempValueCount).toFixed(8) * 1).toString();
+    value.textContent = ''
+    display.textContent = tempValueCount;
+    ans = tempValueCount
+    displayValue = 0;
+    tempValueDisp = '';
+    tempValueCount = 0;
+    multDivCount = 0;
+    inputValue = '';
+    operator = '';
+    multDivOperator = '';
+    comma.addEventListener('click', commaClick);
+});
+
+document.getElementById('clear').addEventListener('click', () => {
+    displayValue = 0;
+    tempValueDisp = '';
+    tempValueCount = 0;
+    multDivCount = 0;
+    inputValue = '';
+    operator = '';
+    multDivOperator = '';
+    display.textContent = displayValue;
+    value.textContent = tempValueDisp;
+    comma.addEventListener('click', commaClick);
+});
 
 const display = document.getElementById('display');
 const value = document.getElementById('value');
@@ -62,29 +119,16 @@ value.textContent = tempValueDisp;
 const numberButtons = document.querySelectorAll('.numbers');
 numberButtons.forEach(button => button.addEventListener('click', numberButtonPress));
 
-const operatorButtons = document.querySelectorAll('.operators');
-operatorButtons.forEach(button => button.addEventListener('click', operatorButtonPress));
+const comma = document.querySelector('.comma');
+comma.addEventListener('click', commaClick);
+    
 
-document.getElementById('equals').addEventListener('click', () => {
-    tempValueCount = functions[operator](Number(tempValueCount), Number(inputValue));
-    value.textContent = ''
-    display.textContent = tempValueCount;
-    displayValue = 0;
-    tempValueDisp = '';
-    tempValueCount = 0;
-    inputValue = '';
-    operator = '';
-});
+const addSubtractButtons = document.querySelectorAll('.addsub');
+addSubtractButtons.forEach(button => button.addEventListener('click', addSubtractPress));
 
-document.getElementById('clear').addEventListener('click', () => {
-    displayValue = 0;
-    tempValueDisp = '';
-    tempValueCount = 0;
-    inputValue = '';
-    operator = '';
-    display.textContent = displayValue;
-    value.textContent = tempValueDisp;
-});
+const multDivButtons = document.querySelectorAll('.multdiv');
+multDivButtons.forEach(button => button.addEventListener('click', multDivPress));
+
 
 
 // var string = document.getElementById('add').dataset.input.toString()
